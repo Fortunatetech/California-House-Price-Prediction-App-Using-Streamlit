6bdfaeb0582753e15ffac98a955829dcbada389f
Ayodele Ayodeji <ayodeleayodeji250@gmail.com>
2023-06-20 10:25:42 -0500

    rename main ipynb note and update About section of the app

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def main():
     st.title("California House Price Prediction App")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit House Prediction ML App </h2>
+    <h2 style="color:white;text-align:center;">Streamlit House Prices Prediction ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
@@ -70,28 +70,46 @@ def main():
     if st.button("About"):
         st.text(" Predictive Analysis Project Using Python")
         st.text(" Background of the dataset")
-        st.text("""The data contains information from the 1990 California census. **So although it may not help you with predicting current housing prices like the Zillow Zestimate dataset, it does provide an accessible introductory dataset for teaching people about the basics of machine learning. The data pertains to the houses found in a given California district and some summary stats about them based on the 1990 census data. Be warned the data aren't cleaned so there are some preprocessing steps required! The columns are as follows, their names are pretty self explanitory:longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value, ocean_proximity.
+        st.text("""
+            The data contains information from the 1990 California census. 
+            So although it may not help you with predicting current housing prices like the Zillow Zestimate dataset,
+            it does provide an accessible introductory dataset for teaching people about the basics of machine learning.
+            The data pertains to the houses found in a given California district and some summary stats 
+            about them based on the 1990 census data. Be warned the data aren't cleaned 
+            so there are some preprocessing steps required! The columns are as follows, 
+            their names are pretty self explanitory:longitude, latitude, housing_median_age, 
+            total_rooms, total_bedrooms, population, households, median_income, median_house_value, 
+            ocean_proximity.
 
-**variable descrition:**
+            variable descrition:
 
- **longitude:** A measure of how far west a house is; a higher value is farther west
- **latitude:** A measure of how far north a house is; a higher value is farther north
- **housingMedianAge:** Median age of a house within a block; a lower number is a newer building
- **totalRooms:** Total number of rooms within a block
- **totalBedrooms:** Total number of bedrooms within a block
- **population:** Total number of people residing within a block
- **households:** Total number of households, a group of people residing within a home unit, for a block
- **medianIncome:** Median income for households within a block of houses (measured in tens of thousands of US Dollars)
- **medianHouseValue:** Median house value for households within a block (measured in US Dollars)
- **oceanProximity:** Location of the house w.r.t ocean/sea""")
+            longitude: A measure of how far west a house is; a higher value is farther west.
 
+            latitude: A measure of how far north a house is; a higher value is farther north.
+
+            housingMedianAge: Median age of a house within a block; a lower number is a newer building.
+
+            totalRooms: Total number of rooms within a block.
+
+            totalBedrooms: Total number of bedrooms within a block.
+
+            population: Total number of people residing within a block.
+
+            households: Total number of households, a group of people residing within a home unit, for a block.
+
+            medianIncome: Median income for households within a block of houses (measured in tens of thousands of US Dollars).
+
+            medianHouseValue: Median house value for households within a block (measured in US Dollars).
+
+            oceanProximity: Location of the house w.r.t ocean/sea.
+            
+                """)
 
               
-
         st.text("Built with Streamlit")
 
 if __name__=='__main__':
     main()
     
     
-    
+  
